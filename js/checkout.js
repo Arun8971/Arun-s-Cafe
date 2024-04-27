5673dbec2b6bcad4c12fcad14860aaa2c3315e1e
Arun8971 <145211502+Arun8971@users.noreply.github.com>
2024-04-27 20:58:31 +0530

    Add files via upload

---
+++ js/checkout.js
@@ -0,0 +1,28 @@
+function buyNow(headId, priceId) {
+
+    let alertbox=document.querySelector('.alert-burger');
+
+    alertbox.style.display='block';
+
+    localStorage.setItem("itemName", headId);
+    localStorage.setItem("itemPrice", priceId);
+}
+
+
+function checkout(){
+    let address=document.getElementById("address").value;
+    if(address){
+        var itemName = localStorage.getItem('itemName');
+        var itemPrice = localStorage.getItem('itemPrice');
+
+        var greeting = `Greetings from Arun's Cafe\n\nYour order: ${itemName}\nPrice: ${itemPrice} is Confirmed..! & We'll deliver it to you soon. Enjoy your meal!\n\nDelivery address: ${address}`;
+
+        alert(greeting);
+    }
+    
+    else{
+        let msg="Please enter valid address for delivery";
+        alert(msg);
+    }
+}
+
