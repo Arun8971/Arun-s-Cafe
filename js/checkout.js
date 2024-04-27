3bbc65e89f648ffea52f318af9dbda7b24a3e0e9
Arun8971 <145211502+Arun8971@users.noreply.github.com>
2024-04-27 21:19:33 +0530

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -18,11 +18,19 @@ function checkout(){
         var greeting = `Greetings from Arun's Cafe\n\nYour order: ${itemName}\nPrice: ${itemPrice} is Confirmed..! & We'll deliver it to you soon. Enjoy your meal!\n\nDelivery address: ${address}`;
 
         alert(greeting);
+
+            let alertbox=document.querySelector('.alert-burger');
+
+            alertbox.style.display='none';
     }
     
     else{
         let msg="Please enter valid address for delivery";
         alert(msg);
+
+            let alertbox=document.querySelector('.alert-burger');
+
+            alertbox.style.display='block';
     }
 }
 
